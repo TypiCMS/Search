f4ac2d6c20504f97fc1bf8696d4475f38289d32a
Samuel De Backer <sdebacker@gmail.com>
2020-10-01 10:24:58 +0200

    Update _form.blade.php

--- src/resources/views/public/_form.blade.php
+++ src/resources/views/public/_form.blade.php
@@ -1,3 +1,4 @@
+@if(Route::has($lang.'::search'))
 <form method="get" action="{{ route($lang.'::search') }}" class="search-form">
     <div class="input-group">
         <input class="search-input form-control" type="text" name="search" id="search" aria-label="@lang('Search')" placeholder="@lang('Search')" value="{{ old('search') }}">
@@ -11,3 +12,4 @@
         </div>
     </div>
 </form>
+@endif
